94723c08adfff1522efc0f0481a2fab4225afeb9
Mahmoud Osman <devmahmoudosman@gmail.com>
2024-09-07 13:07:30 +0300

    continue work from laptop

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:test_advance/core/routing/app_router.dart';
 import 'package:test_advance/doc_app.dart';
 
 void main() {
-  runApp(DocApp(
-    appRouter: AppRouter(),
-  ));
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  runApp(
+    DocApp(
+      appRouter: AppRouter(),
+    ),
+  );
+  FlutterNativeSplash.remove();
 }
